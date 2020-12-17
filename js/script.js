943a148a7a9deb9a7bead458e40af7a69887f98d
Michele Cofano <brutalmachinelol@gmail.com>
2020-12-17 16:13:47 +0100

    Sviluppato il ragionamento (commenti su JS)

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 //Scrivi un programma che stampi i numeri da 1 a 100,
 //ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// --------- RAGIONAMENTO -----------
+// Per i multipli di 3 stampare fizz, quindi i % 3
+// Per i multipli di 5 stampare buzz, quindi i % 5
+// Per i multipli sia di 3 che di 5 (15) stampare fizzbuzz, quindi i % 15
